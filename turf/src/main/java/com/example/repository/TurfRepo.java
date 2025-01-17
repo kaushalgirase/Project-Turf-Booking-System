7700ac98e372120a7e7a20a31f23ccc0b523d669
kaushalgirase <163240788+kaushalgirase@users.noreply.github.com>
2025-01-17 21:39:10 +0530

    Add files via upload

---
+++ turf/src/main/java/com/example/repository/TurfRepo.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entity.TurfDetails;
+
+public interface TurfRepo extends JpaRepository<TurfDetails, Long> {
+
+//	void deleteById(Integer id);
+
+	
+
+}
